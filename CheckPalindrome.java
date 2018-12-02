dea01c1a08f7e999503db51f3c9f0eb658c01df4
Yatharth <yatharth0045@gmail.com>
2018-12-02 10:25:49 +0530

    c

---
+++ CheckPalindrome.java
@@ -0,0 +1,26 @@
+/*
+Given the string, check if it is a palindrome.
+
+Example
+
+For inputString = "aabaa", the output should be
+checkPalindrome(inputString) = true;
+For inputString = "abac", the output should be
+checkPalindrome(inputString) = false;
+For inputString = "a", the output should be
+checkPalindrome(inputString) = true.
+
+*/
+
+class checkPalindrome{
+    public static void main(String[] args){
+        String s="ajayaja";
+        System.out.println(checkPalindrome(s));
+    }
+
+    static boolean checkPalindrome(String inputString) {
+        StringBuffer s=new StringBuffer(inputString);
+        s=s.reverse();
+        return inputString.equals(s.toString());
+    }
+}
